30ce081184e73aa4e820ccea8d1ebc26e49fe255
majd ewawi <majd199ziad@gmail.com>
2020-02-14 13:26:57 +0200

    added database functionality

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+
+var myobj8 = [
+	{ name: 'John', address: 'Highway 71' },
+	{ name: 'Peter', address: 'Lowstreet 4' },
+	{ name: 'Amy', address: 'Apple st 652' },
+	{ name: 'Hannah', address: 'Mountain 21' },
+	{ name: 'Michael', address: 'Valley 345' },
+	{ name: 'Sandy', address: 'Ocean blvd 2' },
+	{ name: 'Betty', address: 'Green Grass 1' },
+	{ name: 'Richard', address: 'Sky st 331' },
+	{ name: 'Susan', address: 'One way 98' },
+	{ name: 'Vicky', address: 'Yellow Garden 2' },
+	{ name: 'Ben', address: 'Park Lane 38' },
+	{ name: 'William', address: 'Central st 954' },
+	{ name: 'Chuck', address: 'Main Road 989' },
+	{ name: 'Viola', address: 'Sideway 1633' }
+];
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/';
 
-MongoClient.connect(url , { useNewUrlParser: true , useUnifiedTopology: true}, function(err, db) {
-    if (err) throw err;
-    console.log("Connected to MongoDB!");
-    
-    
-    db.close();
-  })
+
+// MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+// 	if (err) throw err;
+// 	var dbo = db.db('myproject');
+// 	dbo.createCollection("customers", function (err, res) {
+// 		if (err) throw err;
+// 		console.log("Collection created!");
+// 		var myobj = { name: "Company Inc 2", address: "Highway 37" };
+// 		dbo.collection("customers").insertOne(myobj, function (err, res) {
+// 			if (err) throw err;
+// 			console.log("1 document inserted");
+// 			console.log(res.insertedCount);
+// 		});
+
+// 		dbo.collection("customers").insertMany(myobj8, function (err, res) {
+// 			if (err) throw err;
+// 			console.log("Number of documents inserted: " + res.insertedCount);
+// 			db.close();
+
+// 		});
+// 	});
+// 	console.log("Connected to MongoDB!");
+
+// });
+
+
+
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } , function (err, db) {
+	if (err) throw err;
+	var dbo = db.db("myproject");
+	dbo.collection("customers").find({}).toArray(function (err, result) {
+		if (err) throw err;
+		console.log(result);
+		db.close();
+	});
+});
+
+
+
